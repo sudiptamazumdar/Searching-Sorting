1f753233c8cd9c533548330d2cd0cfa2f257dc64
Sudipta Mazumdar <139207924+sudiptamazumdar@users.noreply.github.com>
2024-06-02 14:24:35 +0530

    Add files via upload

---
+++ optimised_bubbleSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int *, int); // prototype
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];       // input array
+    }
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";  // sorted array output
+    }
+    return 0 ;
+}
+
+void bubbleSort(int *arr, int n)
+{
+    for (int i = 1; i <= n - 1; i++)
+    {
+        bool flag = false;
+        for (int j = 0; j <= n- i - 1; j++ )
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                flag = true;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];   //swapping 
+                arr[j + 1] = temp;
+            }
+        }
+        if (!flag)
+            break;
+    }
+}
